bf93845a4fb1dd474e469a9057311cf8ad326595
John Brennan <john.brennan55@mail.dcu.ie>
2016-04-21 17:18:05 +0100

    only send invoices back for this business

--- app/utils/invoice.js
+++ app/utils/invoice.js
@@ -5,9 +5,12 @@ export function packageInvoiceForStorage(state) {
   let image = state.UploadInvoice.image;
   let items = state.UploadInvoice.items;
   let form = state.UploadInvoice.form;
+  let user = state.authenication.user.username;
+  let businessTo = state.authenication.user.business;
   console.log(JSON.stringify(items.toJS()) +"\n\n\n" + JSON.stringify(form.toJS()));
   return {
     image,
+    user, businessTo,
     form: form.toJS(),
     items: items.toJS()
   };
